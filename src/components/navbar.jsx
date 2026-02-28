fe13595b5ec9013cea8b20d9c21da2432c92dfdc
MurikiSaiSrinivas <msaisrinivas08@gmail.com>
2026-02-27 17:49:47 -0800

    Setup, Config & assets

---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { navLinks } from '../contants'
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple logo" />
+                <ul>
+                    {navLinks.map(({ name, link }) => (
+                        <li key={name}>
+                            <a href={link}>{name}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='flex-center gap-3'>
+                    <button>
+                        <img src="/search.svg" alt="Search icon" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart icon" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
---
+++ src/contants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [{ name: 'Store', link: '#' },
+{ name: 'Mac', link: '#' },
+{ name: 'iPhone', link: '#' },
+{ name: 'Watch', link: '#' },
+{ name: 'Vision', link: '#' },
+{ name: 'AirPods', link: '#' }
+];
