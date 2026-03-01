a938bcd5abe3eab12618af2994bc3ff209bc881c
MurikiSaiSrinivas <msaisrinivas08@gmail.com>
2026-02-28 18:13:48 -0800

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { footerLinks } from '../contants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="info">
+        <p>More ways to shop: <a href="#">Find an Apple Store</a> or other retailer near you. Or Call 000800 040 1966</p>
+      </div>
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
